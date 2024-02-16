ce64616c981f03a07d317e96af31281888e7e706
Dilaxsaswaran <dilaxsaswaran2023@gmail.com>
2024-02-16 21:00:42 +0530

    reverse pramid

---
+++ Java/Easy - Basic Programs/ReversePyramid_Pattern.java
@@ -0,0 +1,26 @@
+/*
+ * Author: Nitharshana18
+ * Email: nitharshana0518@gmail.com
+ * Date: 2024.02.15
+ * Description: This program is to print the reverse pyramid pattern using stars.
+ */
+
+
+ public class ReversePyramid_Pattern {
+	public static void main(String[] args)  
+	{  
+	int rows=8;  
+	for (int i= 0; i<= rows-1; i++)  
+	{  
+	for (int j=0; j<=i; j++)  
+	{  
+	System.out.print(" ");  
+	}  
+	for (int k=0; k<=rows-1-i; k++)  
+	{  
+	System.out.print("*" + " ");  
+	}  
+	System.out.println();  
+	}  
+	}  
+	}  
